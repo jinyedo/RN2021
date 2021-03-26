8a2ebd8ddac739d7127b46ed45f98f2b925c3a13
jinyedo <whereyedo@naver.com>
2021-03-26 14:53:56 +0900

    실습

--- App.js
+++ App.js
@@ -28,18 +28,44 @@ import {
 
 class App extends React.Component {
   
-  state = {
-    year: 23,
-    name: "Jin Ye Do",
-    colors: ['blue']
+  constructor() {
+    super();
+    this.state = {
+      year: 23,
+      leapYear: true,
+      name: "Jin Ye Do",
+      colors: ['blue'],
+      topics: ['React', 'React Native', 'JavaScript'],
+      info: {
+        paperback: true,
+        length: '335 pages',
+        type: 'programming'
+      }
+    }
+  }
+
+  updateYear() {
+    this.setState({
+      year: 24
+    })
   }
 
   render() {
+    let leapyear = <Text>This is not a leapYear!</Text>
+    if (this.state.leapYear) {
+      leapyear = <Text>THis is a leapYear!</Text>
+    }
     return(
       <View>
-        <Text>My Name is: {this.state.name}</Text>
-        <Text>My year is: {this.state.year}</Text>
-        <Text>My color is: {this.state.colors}</Text>
+        <Text>My Name is: { this.state.name }</Text>
+        <Text onPress={() => this.updateYear()}>
+          My year is: { this.state.year }
+        </Text>
+        <Text>My color are: { this.state.colors[0] }</Text>
+        <Text>Topics: { this.state.topics[0] }</Text>
+        <Text>Length: { this.state.info.length }</Text>
+        <Text>Type: { this.state.info.type }</Text>
+        { leapyear }
       </View>
     )
   }
--- README.md
+++ README.md
@@ -1,1 +1,44 @@
 "# RN2021" 
+
+# 0326
+
+### 컴포넌트 생명주기 - life cycle method
+```
+1. constructor() - 생성자 | 제일먼저 실행됨
+2. componentDidMount() - render() 다음으로 실행
+3. 실행순서: constructor() -> render() -> componentDidMount() 
+
+4. componentDidUpdate() - 화면 업데이트시 실행 
+5. componentWillMount() - 컴포넌트 종료시 실행
+```
+
+### state 사용하기
+
+```
+1. "state"를 직접 변경할 수 없다: 원래 리엑트는 "state"가 변경되면 render()를 다시 실행하여 변경된 "state"를 화면에 보여주지만 "state"를 직접 변경하면 render() 함수를 다시 실행해 주지 않음
+
+2. setState() 함수를 사용하면 "state"를 변경할 수 있고. 바뀐 "state"의 데이터만 업데이트한다.
+
+ex)
+  constructor() {
+    super();
+    this.state = {
+      year: 23
+    }
+  }
+
+  updateYear() {
+    this.setState({year: 24})
+  }
+```
+
+### onPredd() : 클릭 이벤트 처리
+```
+ex) 클릭시 updateYear() 호출
+<Text onPress={() => this.updateYear()}>
+    My year is: {this.state.year}
+</Text>
+```
+
+
+---
